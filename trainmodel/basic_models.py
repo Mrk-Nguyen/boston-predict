33146c943c7637433e2f366aa05f0caec31d2341
Mark Nguyen <Mrk-Nguyen@users.noreply.github.com>
2021-04-06 18:05:04 -0700

    Add basic models (#3)
    * Add basic models
    
    * Add docstrings to all classes

--- trainmodel/basic_models.py
+++ trainmodel/basic_models.py
@@ -2,26 +2,31 @@ import numpy as np
 from collections.abc import Iterable
 
 # Build base class model
+'''Basic doc string'''
 class MeanRegressor(object):
     def __init__(self):
         super().__init__()
         self._mean = 0
     
     def fit(self: object, X: Iterable, Y: Iterable) -> object:
+        self._mean = np.mean(Y)
         return self
     
     def predict(self: object, X: Iterable) -> Iterable:
-        return []
+        return np.full(len(X), self._mean)
 
 # Build random regressor model
 class RandomRegressor(object):
+'''Basic doc string'''
     def __init__(self):
         super().__init__()
         self._max = 100
         self._min = 0
     
     def fit(self: object, X: Iterable, Y: Iterable) -> object:
+        self._max, self._min = max(Y), min(Y)
         return self
     
     def predict(self: object, X: Iterable) -> Iterable:
-        return []
+        np.random.seed(291)
+        return np.random.rand(len(X)) * (self._max - self._min) + self._min

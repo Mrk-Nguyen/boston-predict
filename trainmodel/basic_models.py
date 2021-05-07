9d354338938938695c5fc77e178a4aca06f1d2cb
mengran116 <83249927+mengran116@users.noreply.github.com>
2021-05-07 15:20:43 -0700

    Feature dev2 scoreendpoint (#10)
    * Mengran replaced results function

--- trainmodel/basic_models.py
+++ trainmodel/basic_models.py
@@ -3,25 +3,30 @@ from collections.abc import Iterable
 
 # Build base class model
 class MeanRegressor(object):
+'''Basic mean regressor class for Naive models'''
     def __init__(self):
         super().__init__()
         self._mean = 0
     
     def fit(self: object, X: Iterable, Y: Iterable) -> object:
+        self._mean = np.mean(Y)
         return self
     
     def predict(self: object, X: Iterable) -> Iterable:
-        return []
+        return np.full(len(X), self._mean)
 
 # Build random regressor model
 class RandomRegressor(object):
+'''Basic random regressor class for Naive models'''
     def __init__(self):
         super().__init__()
         self._max = 100
         self._min = 0
     
     def fit(self: object, X: Iterable, Y: Iterable) -> object:
+        self._max, self._min = max(Y), min(Y)
         return self
     
     def predict(self: object, X: Iterable) -> Iterable:
-        return []
+        np.random.seed(291)
+        return np.random.rand(len(X)) * (self._max - self._min) + self._min
